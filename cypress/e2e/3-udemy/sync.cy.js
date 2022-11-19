3d8b98ab369f691e660af4bae6688f5ef833fe02
maNNIakk <renato.yancovit4@gmail.com>
2022-11-18 22:55:49 -0300

    Testing itens with delay

--- cypress/e2e/3-udemy/sync.cy.js
+++ cypress/e2e/3-udemy/sync.cy.js
@@ -18,9 +18,31 @@ describe('Esperas', () => {
         cy.get('#novoCampo').type('Funcionou');
     })
 
-    it.only('Deve fazer retrys', () => {
+    it('Deve fazer retrys', () => {
+        cy.get('#novoCampo').should('not.exist');
         cy.get('#buttonDelay').click();
-        cy.get('#novoCampo').should('exist');
+        cy.get('#novoCampo').should('not.exist');
+        cy.get('#novoCampo')
+           .should('exist')
+            .type('Funciona')
+        //.should('not.exist')
+    })
+
+    it('Pegar itens com delay 1', () => {
+        cy.get('#lista').should('not.exist');
+        cy.get('#buttonList').click();
+        cy.get('#lista').should('exist');
+        cy.contains('span','Item 1').should('exist');
+        cy.contains('span','Item 2').should('exist');
+    })
+
+    it.only('Pegar itens com delay 2', () => {
+        cy.get('#buttonList').click();
+        cy.get('#lista li')
+           .find('span')
+            .should('contain','Item 1')
+        cy.get('#lista li span')
+            .should('contain','Item 2')
     })
 
 })
