38543913da18e3fce95f31d9f6b0bd089cd2b517
maNNIakk <renato.yancovit4@gmail.com>
2022-11-20 18:35:09 -0300

    ShoulxThen w/ wraps and promises

---
+++ cypress/e2e/3-udemy/helpers.cy.js
@@ -0,0 +1,30 @@
+///<reference types="cypress"/>
+
+describe('Helpers...', () => {
+    it('Wrap', () => {
+        const obj = {nome:'User', idade: 20};
+        expect(obj).to.have.property('nome');
+        cy.wrap(obj).should('have.property','nome');
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+        // cy.get('#formNome').then($el => {
+        //     cy.wrap($el).type('funciona');
+        //     // $el.type('funciona??')
+        // })
+
+        const promise = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(10)
+            }, 500)
+        })
+
+    cy.get('#buttonSimple').then(() => console.log('ENcontrei o primeiro'))
+    // promise.then(num => console.log(num))
+    cy.wrap(promise).then(ret => console.log(ret))
+    cy.get('#buttonList').then(() => console.log('ENcontrei o segundo'))
+
+    cy.wrap(1).then(num => {
+        return 2
+        }).should('be.equal',2)
+    })
+})
