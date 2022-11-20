332b2a3201ac88210d59fd5c4489626bae63257b
maNNIakk <renato.yancovit4@gmail.com>
2022-11-20 18:45:38 -0300

    Wraping things and its+its

--- cypress/e2e/3-udemy/helpers.cy.js
+++ cypress/e2e/3-udemy/helpers.cy.js
@@ -27,4 +27,16 @@ describe('Helpers...', () => {
         return 2
         }).should('be.equal',2)
     })
+
+    it('Its...', () => {
+        const obj = {nome:'User', idade: 20};
+
+        cy.wrap(obj).should('have.property','nome','User')
+        cy.wrap(obj).its('nome').should('be.equal','User')
+        const obj2 = {nome:'User', idade: 20, endereco: { rua:'rua dos bobos'}};
+        cy.wrap(obj2).its('endereco.rua').should('be.equal', 'rua dos bobos');
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal', 20)
+    })
 })
